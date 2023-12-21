10847c7f9c204d9c8990b929a611c383882d7adb
aothymoon59 <aothymoon59@gmail.com>
2023-12-22 03:15:56 +0600

    fix bug in review model and valdation schema

--- src/app/modules/Review/review.model.ts
+++ src/app/modules/Review/review.model.ts
@@ -3,7 +3,7 @@ import { TReview } from './review.interface';
 
 const reviewSchema = new Schema<TReview>({
   courseId: { type: Schema.Types.ObjectId, required: true, ref: 'Course' },
-  rating: { type: Number, required: true },
+  rating: { type: Number, min: 1, max: 5, required: true },
   review: { type: String, required: true },
 });
 
--- src/app/modules/Review/review.validation.ts
+++ src/app/modules/Review/review.validation.ts
@@ -6,10 +6,13 @@ const reviewValidationSchema = z.object({
       invalid_type_error: 'courseId must be string',
       required_error: 'courseId is required',
     }),
-    rating: z.number({
-      invalid_type_error: 'rating must be number',
-      required_error: 'rating is number',
-    }),
+    rating: z
+      .number({
+        invalid_type_error: 'rating must be number',
+        required_error: 'rating is number',
+      })
+      .min(1)
+      .max(5),
     review: z.string({
       invalid_type_error: 'review must be string',
       required_error: 'review is required',
