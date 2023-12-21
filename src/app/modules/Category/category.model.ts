e4c99c32cf5d7c3c8414b54208818a106a6455d2
aothymoon59 <aothymoon59@gmail.com>
2023-12-22 02:25:29 +0600

    review model and review service created

--- src/app/modules/Category/category.model.ts
+++ src/app/modules/Category/category.model.ts
@@ -2,7 +2,11 @@ import { Schema, model } from 'mongoose';
 import { TCategory } from './category.interface';
 
 const categorySchema = new Schema<TCategory>({
-  name: { type: String, required: [true, 'name is required'], unique: true },
+  name: {
+    type: String,
+    required: [true, 'Category name is required'],
+    unique: true,
+  },
 });
 
 export const Category = model<TCategory>('Category', categorySchema);
